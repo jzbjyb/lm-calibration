dadec2e0136c2f67abbb0628cbaa8daec790013a
jzb <zhengbaj@cs.cmu.edu>
2020-12-09 06:26:26 -0500

    add max token per batch

--- model/gpt2.py
+++ model/gpt2.py
@@ -5,12 +5,60 @@ from torch.nn.utils.rnn import pad_sequence
 
 
 def string_to_tensor(tokenizer,
-                     inputs: List[str],
-                     targets: List[str],
+                     data,
                      max_input_len: int,
                      max_target_len: int,
                      add_eos: bool=True,
+                     pad_to_max: bool=False,
+                     max_token_per_batch: int=None,
                      device: str='cuda') -> Tuple[Dict[str, torch.Tensor], List[int]]:
+  combine = {'input_ids': [], 'attention_mask': []}
+  split_point = []
+  max_len_per_example: int = 0
+  for input, target in data:
+    # tokenize
+    t_input = tokenizer(input)
+    t_target = tokenizer(target)
+    # truncate
+    inp = t_input['input_ids'][:max_input_len]
+    tar = t_target['input_ids'][:max_target_len - int(add_eos)]
+    inp_att = t_input['attention_mask'][:max_input_len]
+    tar_att = t_target['attention_mask'][:max_target_len - int(add_eos)]
+    # add eos
+    if add_eos:
+      tar.append(tokenizer.eos_token_id)
+      tar_att.append(1)
+    # combine
+    comb_ids = inp + tar
+    comb_mask = inp_att + tar_att
+    # padding
+    if pad_to_max:
+      assert len(comb_ids) <= max_input_len + max_target_len and len(comb_ids) == len(comb_mask)
+      comb_ids += [0] * (max_input_len + max_target_len - len(comb_ids))
+      comb_mask += [0] * (max_input_len + max_target_len - len(comb_mask))
+    # yield or not
+    cur_len = len(comb_ids)
+    if max(cur_len, max_len_per_example) * (len(combine['input_ids']) + 1) > max_token_per_batch:
+      combine['input_ids'] = pad_sequence([torch.LongTensor(i) for i in combine['input_ids']],
+                                          batch_first=True, padding_value=0).to(device)
+      combine['attention_mask'] = pad_sequence([torch.LongTensor(i) for i in combine['attention_mask']],
+                                               batch_first=True, padding_value=0).to(device)
+      yield combine, split_point
+      combine = {'input_ids': [], 'attention_mask': []}
+      split_point = []
+      max_len_per_example = 0
+    # save
+    combine['input_ids'].append(comb_ids)
+    combine['attention_mask'].append(comb_mask)
+    split_point.append(len(inp))
+    max_len_per_example = max(max_len_per_example, cur_len)
+  if len(combine['input_ids']) > 0:
+    combine['input_ids'] = pad_sequence([torch.LongTensor(i) for i in combine['input_ids']],
+                                        batch_first=True, padding_value=0).to(device)
+    combine['attention_mask'] = pad_sequence([torch.LongTensor(i) for i in combine['attention_mask']],
+                                             batch_first=True, padding_value=0).to(device)
+    yield combine, split_point
+  '''
   assert len(inputs) == len(targets)
   t_inputs = tokenizer(inputs)
   t_targets = tokenizer(targets)
@@ -18,20 +66,27 @@ def string_to_tensor(tokenizer,
   split_point = []
   for i in range(len(inputs)):
     inp = t_inputs['input_ids'][i][:max_input_len]
-    tar = t_targets['input_ids'][i][:max_target_len]
+    tar = t_targets['input_ids'][i][:max_target_len - int(add_eos)]
     inp_att = t_inputs['attention_mask'][i][:max_input_len]
-    tar_att = t_targets['attention_mask'][i][:max_target_len]
+    tar_att = t_targets['attention_mask'][i][:max_target_len - int(add_eos)]
     if add_eos:
       tar.append(tokenizer.eos_token_id)
       tar_att.append(1)
-    combine['input_ids'].append(inp + tar)
-    combine['attention_mask'].append(inp_att + tar_att)
+    comb_ids = inp + tar
+    comb_mask = inp_att + tar_att
+    if i == 0 and pad_to_max:
+      assert len(comb_ids) <= max_input_len + max_target_len and len(comb_ids) == len(comb_mask)
+      comb_ids += [0] * (max_input_len + max_target_len - len(comb_ids))
+      comb_mask += [0] * (max_input_len + max_target_len - len(comb_mask))
+    combine['input_ids'].append(comb_ids)
+    combine['attention_mask'].append(comb_mask)
     split_point.append(len(inp))
   combine['input_ids'] = pad_sequence([torch.LongTensor(i) for i in combine['input_ids']],
                                       batch_first=True, padding_value=0).to(device)
   combine['attention_mask'] = pad_sequence([torch.LongTensor(i) for i in combine['attention_mask']],
                                            batch_first=True, padding_value=0).to(device)
   return combine, split_point
+  '''
 
 
 def compute_logprob(model, input_dict: Dict[str, torch.Tensor]):
--- score_pytorch.py
+++ score_pytorch.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from tqdm import tqdm
 import numpy as np
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
 from model.data import read_qa_data
@@ -9,15 +10,23 @@ CLEAN_TEST_DOMAINS = [('arc_hard', ('train', 'dev', 'test')),
                       ('mctest_corrected_the_separator', ('train', 'dev')),
                       ('social_iqa', ('train', 'dev')),
                       ('race_string', ('train', 'dev', 'test'))]
-
+CLEAN_TRAIN_DOMAINS = [('arc_easy', ('train', 'dev', 'test')),
+                       ('ai2_science_elementary', ('train', 'dev', 'test')),
+                       ('openbookqa', ('train', 'dev', 'test')),
+                       ('qasc', ('train', 'dev', 'test')),
+                       ('winogrande_l', ('train', 'dev')),
+                       ('commonsenseqa', ('train', 'dev', 'test')),
+                       ('physical_iqa', ('train', 'dev', 'test'))]
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='score targets using models from transformers')
-  parser.add_argument('--model', type=str, help='model name', default='gpt2')
+  parser.add_argument('--model', type=str, help='model name', default='gpt2-xl')
   parser.add_argument('--data', type=str, help='path to the data')
-  parser.add_argument('--split', type=str, help='split')
+  parser.add_argument('--domains', type=str, default='clean_test_domains')
+  parser.add_argument('--split', type=str, help='split', default='dev')
   parser.add_argument('--output', type=str, help='output file')
-  parser.add_argument('--batch_size', type=int, help='batch size', default=32)
+  parser.add_argument('--batch_size', type=int, help='batch size', default=0)
+  parser.add_argument('--max_token_per_batch', type=int, default=1024)
   parser.add_argument('--has_ret', action='store_true')
   parser.add_argument('--use_inp', action='store_true')
   args = parser.parse_args()
@@ -28,19 +37,22 @@ if __name__ == '__main__':
   print('loading models ...')
   model = GPT2LMHeadModel.from_pretrained(args.model).to(device)
   tokenizer = GPT2TokenizerFast.from_pretrained(args.model)
-
-  data = read_qa_data(args.data, CLEAN_TEST_DOMAINS, split='dev', has_ret=args.has_ret, use_inp=args.use_inp)
+  print('init data')
+  data = read_qa_data(args.data, eval(args.domains.upper()), split=args.split, has_ret=args.has_ret, use_inp=args.use_inp)
+  iter = string_to_tensor(tokenizer, data, max_input_len=max_input_len, max_target_len=max_target_len,
+                          add_eos=True, pad_to_max=False, max_token_per_batch=args.max_token_per_batch, device=device)
 
   os.makedirs(os.path.dirname(args.output), exist_ok=True)
+  pbar = tqdm()
   with open(args.output, 'w') as fout:
-    while True:
-      inputs, targets = list(zip(*[next(data) for i in range(args.batch_size)]))
-      inputs, targets = list(inputs), list(targets)
-      input_dict, split_point = string_to_tensor(tokenizer, inputs, targets, max_input_len=max_input_len, max_target_len=max_target_len, add_eos=True, device=device)
+    for input_dict, split_point in iter:
       logprobs = compute_logprob(model, input_dict)
-      for inp, tar, tokens, lp, sp in zip(inputs, targets, input_dict['input_ids'].cpu().numpy(), logprobs.cpu().numpy(), split_point):
+      for tokens, lp, sp in zip(input_dict['input_ids'].cpu().numpy(), logprobs.cpu().numpy(), split_point):
         inp_toks, tar_toks = tokens[:sp], tokens[sp:]
+        #print(tokenizer.convert_ids_to_tokens(inp_toks), tokenizer.convert_ids_to_tokens(tar_toks))
         tar_lp = lp[sp:]
         score = np.sum(tar_lp * (tar_toks > 0))
         fout.write('{}\t{}\t{}\t{}\n'.format(
           score, ','.join(map(str, inp_toks)), ','.join(map(str, tar_toks)), ','.join(map(str, tar_lp))))
+        pbar.update(1)
+  pbar.close()
